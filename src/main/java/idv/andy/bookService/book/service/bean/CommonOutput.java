5eda670ad70cc44232236a069490791ea6204f45
Andy <abc623979@gmail.com>
2022-04-17 02:33:23 +0800

    新增共用回傳物件

---
+++ src/main/java/idv/andy/bookService/book/service/bean/CommonOutput.java
@@ -0,0 +1,11 @@
+package idv.andy.bookService.book.service.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CommonOutput {
+    private boolean isSuccess;
+    private String message;
+}
