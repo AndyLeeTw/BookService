adde268ce66541a9bc3b9cdffa32a8e0ca1fb5c8
Andy <abc623979@gmail.com>
2022-04-16 22:49:24 +0800

    調整啟動 java 位置

--- src/main/java/idv/andy/bookService/BookServiceApplication.java
+++ src/main/java/idv/andy/bookService/BookServiceApplication.java
@@ -1,4 +1,4 @@
-package idv.andy.bookService.main;
+package idv.andy.bookService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
