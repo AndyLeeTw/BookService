167da417d24ab3d61ab23cf7997e79bd5a8d6f5c
Andy <abc623979@gmail.com>
2022-04-17 15:13:11 +0800

    調整 DB 欄位型態及空值判斷

--- src/main/java/idv/andy/bookService/book/dataModel/Book.java
+++ src/main/java/idv/andy/bookService/book/dataModel/Book.java
@@ -2,20 +2,28 @@ package idv.andy.bookService.book.dataModel;
 
 import java.sql.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @Entity
 public class Book {
     @Id
     private String isbn;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false)
     private String author;
     private String translator;
+    @Column(nullable = false)
     private String publisher;
+    @Column(nullable = false)
     private Date publishDate;
-    private String price;
+    @Column(nullable = false)
+    private Double price;
 }
